a80f186a6b7dbfdca5e9bdebde36217d7d3509b4
akhil4ph <akhilanangjha@gmail.com>
2024-10-14 11:05:15 +0530

    Portfolio
    This is my portfolio, which I have prepared during the internship program given by Codsoft.

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-let display = document.getElementById('display');
-let currentInput = '';
-let currentOperation = null;
+let cvBtn = document.getElementById("cvBtn");
+let submit = document.getElementById("submit");
+let bar = document.getElementById("bar");
 
-function clearDisplay() {
-    currentInput = '';
-    currentOperation = null;
-    display.innerText = '0';
-}
+cvBtn.addEventListener("click", function(){
+    alert("Downloded succesfully")
+})
 
-function appendNumber(number) {
-    if (currentInput === '0') {
-        currentInput = number;
-    } else {
-        currentInput += number;
-    }
-    display.innerText = currentInput;
-}
 
-function appendOperation(operation) {
-    if (currentInput === '' && operation !== '-') {
-        return;
-    }
-    if (currentOperation !== null) {
-        calculate();
-    }
-    currentInput += ` ${operation} `;
-    currentOperation = operation;
-    display.innerText = currentInput;
-}
+submit.addEventListener("click", function(){
 
-function calculate() {
-    try {
-        const result = eval(currentInput);
-        display.innerText = result;
-        currentInput = result.toString();
-        currentOperation = null;
-    } catch {
-        display.innerText = 'Error';
+    let email = document.getElementById("email");
+    let pass = document.getElementById("pass");
+
+    if(email.value == "" && pass.value == ""){
+        alert("Fill Detail")
+    }else{
+        alert("Logged IN")
     }
-}
+})
+
+let ul = document.querySelector("ul");
+bar.addEventListener("click", function(){
+    ul.classList.toggle("showData");
+
+    if(ul.className == "showData"){
+        bar.className= "fa-solid fa-xmark"
+    }else{
+        bar.className= "fa-solid fa-bars"
+    }
+})
